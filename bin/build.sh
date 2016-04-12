5ed9e013a2284b6f89c4e83f55145e855ea49eab
Marius Stanciu - Sergiu <marius@vox.space>
2016-04-12 20:44:07 +0300

    added google closure compiler and updated yii compressor that will be used just for stylesheets

--- bin/build.sh
+++ bin/build.sh
@@ -23,6 +23,8 @@ if [ "$command" == "all" -o "$command" == "css" ];
 	then
 		echo - Compressing stylesheets
 		cat bin/css.filelist | xargs cat >> dist/app.css
+		echo - Compiling stylesheets
+		java -jar bin/yuicompressor-2.4.8.jar dist/app.css -o dist/app.min.css -v 2> bin/css.log
 fi
 
 if [ "$command" == "all" -o "$command" == "libs" -o "$command" == "js" ];
@@ -30,7 +32,8 @@ if [ "$command" == "all" -o "$command" == "libs" -o "$command" == "js" ];
 		echo - Compressing libraries
 		cat bin/library.filelist | xargs cat >> dist/lib.js
 		echo - Compiling libraries
-		java -jar bin/yuicompressor-2.4.7.jar dist/lib.js -o dist/lib.min.js -v 2> bin/lib.log
+		java -jar bin/google-closure-compiler.jar --js=dist/lib.js --js_output_file=dist/lib.min.js 2> bin/lib.log
+		# java -jar bin/yuicompressor-2.4.8.jar dist/lib.js -o dist/lib.min.js -v 2> bin/lib.log
 fi
 
 if [ "$command" == "all" -o "$command" == "app" -o "$command" == "js" ];
@@ -38,7 +41,8 @@ if [ "$command" == "all" -o "$command" == "app" -o "$command" == "js" ];
 		echo - Compressing application
 		cat bin/app.filelist | xargs cat >> dist/app.js
 		echo - Compiling application
-		java -jar bin/yuicompressor-2.4.7.jar dist/app.js -o dist/app.min.js -v 2> bin/app.log
+		java -jar bin/google-closure-compiler.jar --js=dist/app.js --js_output_file=dist/app.min.js 2> bin/app.log
+		# java -jar bin/yuicompressor-2.4.8.jar dist/app.js -o dist/app.min.js -v 2> bin/app.log
 fi
 
 echo =============== Done ==================================================
