0bbb821e6fb252c44bd4593aec8a6351d6b12f6c
Marius Stanciu - Sergiu <marius@vox.space>
2016-04-12 20:25:30 +0300

    updated the build file to do some proper ... parametrized building

--- bin/build.sh
+++ bin/build.sh
@@ -1,17 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -rf dist/*
+if [ $# -eq 0 ];
+	then
+		echo "Usage: build.sh SECTION (where section can be one of the following: all, clean, libs, app, js, css"
+		exit 1
+fi
 
-echo - Concatenating
+echo =============== Setting up build environment ===============
 
-cat bin/css.filelist | xargs cat >> dist/app.css
+command="$1"
 
-cat bin/library.filelist | xargs cat >> dist/lib.js
+if [ "$command" == "all" -o "$command" == "clean" ];
+	then
+		echo - Cleaning up previous builds
+		rm -rf dist/*.js
+		rm -rf dist/*.css
+fi
 
-cat bin/app.filelist | xargs cat >> dist/app.js
+echo =============== Compressing files ===============
 
-echo - Compressing
+if [ "$command" == "all" -o "$command" == "css" ];
+	then
+		echo - Compressing stylesheets
+		cat bin/css.filelist | xargs cat >> dist/app.css
+fi
 
-java -jar bin/yuicompressor-2.4.7.jar dist/app.js -o dist/app.min.js -v 2> bin/app.log
+if [ "$command" == "all" -o "$command" == "libs" -o "$command" == "js" ];
+	then
+		echo - Compressing libraries
+		cat bin/library.filelist | xargs cat >> dist/lib.js
+		echo - Compiling libraries
+		java -jar bin/yuicompressor-2.4.7.jar dist/lib.js -o dist/lib.min.js -v 2> bin/lib.log
+fi
 
-rm -rf dist/app.js
+if [ "$command" == "all" -o "$command" == "app" -o "$command" == "js" ];
+	then
+		echo - Compressing application
+		cat bin/app.filelist | xargs cat >> dist/app.js
+		echo - Compiling application
+		java -jar bin/yuicompressor-2.4.7.jar dist/app.js -o dist/app.min.js -v 2> bin/app.log
+fi
+
+echo =============== Done ==================================================
