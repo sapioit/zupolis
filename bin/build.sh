7d5d20ea8559168e4e4196643f9c73ce5690eb1f
Marius Stanciu - Sergiu <marius@vox.space>
2016-04-12 21:07:32 +0300

    moved compressors to separate directory, created missing logs directory

--- bin/build.sh
+++ bin/build.sh
@@ -24,7 +24,7 @@ if [ "$command" == "all" -o "$command" == "css" ];
 		echo - Compressing stylesheets
 		cat bin/css.filelist | xargs cat >> dist/app.css
 		echo - Compiling stylesheets
-		java -jar bin/yuicompressor-2.4.8.jar dist/app.css -o dist/app.min.css -v 2> bin/css.log
+		java -jar vendor/compressors/yuicompressor-2.4.8.jar dist/app.css -o dist/app.min.css -v 2> logs/css.log
 fi
 
 if [ "$command" == "all" -o "$command" == "lib" -o "$command" == "js" ];
@@ -32,8 +32,8 @@ if [ "$command" == "all" -o "$command" == "lib" -o "$command" == "js" ];
 		echo - Compressing libraries
 		cat bin/library.filelist | xargs cat >> dist/lib.js
 		echo - Compiling libraries
-		java -jar bin/google-closure-compiler.jar --js=dist/lib.js --js_output_file=dist/lib.min.js 2> bin/lib.log
-		# java -jar bin/yuicompressor-2.4.8.jar dist/lib.js -o dist/lib.min.js -v 2> bin/lib.log
+		java -jar vendor/compressors/google-closure-compiler.jar --js=dist/lib.js --js_output_file=dist/lib.min.js 2> logs/lib.log
+		# java -jar vendor/compressors/yuicompressor-2.4.8.jar dist/lib.js -o dist/lib.min.js -v 2> logs/lib.log
 fi
 
 if [ "$command" == "all" -o "$command" == "app" -o "$command" == "js" ];
@@ -41,8 +41,8 @@ if [ "$command" == "all" -o "$command" == "app" -o "$command" == "js" ];
 		echo - Compressing application
 		cat bin/app.filelist | xargs cat >> dist/app.js
 		echo - Compiling application
-		java -jar bin/google-closure-compiler.jar --js=dist/app.js --js_output_file=dist/app.min.js 2> bin/app.log
-		# java -jar bin/yuicompressor-2.4.8.jar dist/app.js -o dist/app.min.js -v 2> bin/app.log
+		java -jar vendor/compressors/google-closure-compiler.jar --js=dist/app.js --js_output_file=dist/app.min.js 2> logs/app.log
+		# java -jar vendor/compressors/yuicompressor-2.4.8.jar dist/app.js -o dist/app.min.js -v 2> logs/app.log
 fi
 
 echo =============== Done ==================================================
